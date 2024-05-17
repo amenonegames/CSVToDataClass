2a59658a2e11900a08bb6ab8b438e58fc0bdc8d9
amenonegames <amenone.games@gmail.com>
2024-05-18 01:36:42 +0900

    不要クラスのコメントアウト

--- Assets/Packages/DataClassGenerator/Samples/UseCase/UseTest.cs
+++ Assets/Packages/DataClassGenerator/Samples/UseCase/UseTest.cs
@@ -1,11 +1,11 @@
-﻿using Data;
-
-namespace DefaultNamespace
-{
-    public class UseTest
-    {
-        private Sample2 test;
-
-        // Sample2 sample2 = new Sample2();
-    }
-}
+﻿// using Data;
+//
+// namespace DefaultNamespace
+// {
+//     public class UseTest
+//     {
+//         private Sample2 test;
+//
+//         // Sample2 sample2 = new Sample2();
+//     }
+// }
